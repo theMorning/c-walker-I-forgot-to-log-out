82f3fd61166536c173d671a14255303c779676ea
Student <student@lab-walkin-015.cs.csubak.edu>
2013-04-08 17:40:55 -0700

    When a noob gives you access, make fun.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// user name
+// 8 April 2013
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  for(int i=0; i<9001; i++) {
+      cout << "I am a noob :(\n";
+  }
+  
+  return 0;
+}
